3d9b509755ea3c0c7115c374fb0dafd9b01fdb38
syyong <syy@gmx.com>
2020-06-14 10:10:44 +0100

    Update tests.

--- test_setup_db.py
+++ test_setup_db.py
@@ -3,12 +3,12 @@ from setup_db import check_if_db_exists
 
 def test_n_to_recreate_db(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda *args: "n")
-    assert check_if_db_exists() == 'Not recreating the database'
+    assert check_if_db_exists() == 'Not recreating the database. Exiting.'
 
 
 def test_y_to_recreate_db(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda *args: "y")
-    assert check_if_db_exists() == 'Database recreated'
+    assert check_if_db_exists() == 'Database recreated.'
 
 
 def test_invalid_input_to_recreate(monkeypatch):
